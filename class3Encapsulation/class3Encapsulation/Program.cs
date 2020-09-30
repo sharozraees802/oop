69d78e80b1536bf815345786e90ae4e6b78d9290
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-09-30 12:48:17 +0500

    encapsulation

---
+++ class3Encapsulation/class3Encapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using class3Encapsulation.model.studentmanagement;
+
+namespace class3Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student std = new student();
+            std.SetAge(-10);
+            std.SetName("");
+            std.detail();
+           
+        }
+    }
+}
---
+++ class3Encapsulation/class3Encapsulation/model/studentmanagement/student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class3Encapsulation.model.studentmanagement
+{
+    class student
+    {
+        private int age;
+        private string name;
+
+        public void SetAge (int a)
+        {
+            if (a > 0)
+            {
+                age = a;
+            }
+            else
+            {
+                Console.WriteLine("you've  entered an invalid value,please try again");
+            }
+        }
+
+        public void SetName(string n)
+        {
+            if (!string.IsNullOrEmpty(n))
+            {
+                name = n;
+            }
+            else
+            {
+                Console.WriteLine("Can not enter empty or null value in name");
+            }
+        }
+        public void detail()
+        {
+            Console.WriteLine("your name is {0} and your age is {1}",name,age);
+            Console.ReadLine();
+        }
+
+    }
+}
